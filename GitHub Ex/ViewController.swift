949012e066f3596eae63080419a2d5410222f619
Tyrone <tyroned3@learn.cssd.ab.ca>
2018-11-09 12:47:45 -0700

    initial commit

---
+++ GitHub Ex/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitHub Ex
+//
+//  Created by Tyrone Dizon on 2018-11-08.
+//  Copyright © 2018 Tyrone Dizon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
